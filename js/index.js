fd6a85dca12d1de2e5088728c2463c61e27d6db6
Hidingherotw <150829848+Hidingherotw@users.noreply.github.com>
2023-11-16 00:21:51 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,41 +6,45 @@ window.onload = function(){
     const btn1 = document.querySelector("#btn1");
     const btn2 = document.querySelector("#btn2");
     const sharetBtn = document.querySelector("#share-btn");
+
+    const firstPage = 0;
+    const keyPage = 2;
+    const lastPage = 4;
+
     let page = 0;
+    let recordPage = keyPage;
     let answerArr = [];
 
     function turnPage(answer){
-        if (page == 0){
-            bgm.play();
-            startBtn.style.display = "none";
-            btn1.style.display = "block";
-            btn2.style.display = "block";
-
-            page = 1;
-        } else if (page == 1){
-            page = 2;
-        } else if (page == 2){
-            answerArr.push(answer);
-
-            page += answer;
-        } else {
+        if (recordPage== page){
             answerArr.push(answer);
         }
+
+        switch (page){
+            case firstPage:
+                start();
+                break;
+
+            case keyPage:
+                recordPage = recordPage + answer;
+                break;
+        }
+        page++;
 
         bg.src = `./src/image/q${page}-bg.png`;
         btn1.style.backgroundImage = `url("./src/image/q${page}-btn1.png")`;
         btn2.style.backgroundImage = `url("./src/image/q${page}-btn2.png")`;
 
-        if (answerArr.length >= 2){
-            btn1.style.display = "none";
-            btn2.style.display = "none";
-            sharetBtn.style.display = "block";
-
-            result = parseResult(answerArr);
-
-            body.style.backgroundColor = "#fce56d";
-            bg.src = `./src/image/role-${result}.png`;
+        if (page > lastPage){
+            showResult(answerArr);
         }
+    }
+
+    function start(){
+        bgm.play();
+        startBtn.style.display = "none";
+        btn1.style.display = "block";
+        btn2.style.display = "block";
     }
 
     function parseResult(answerArr){
@@ -50,26 +54,39 @@ window.onload = function(){
             case "1,1":
                 result = "NT";
                 break;
+
             case "1,2":
                 result = "NF";
                 break;
+
             case "2,1":
                 result = "SJ";
                 break;
+
             case "2,2":
                 result = "SP";
                 break;
         }
-        
         return result;
     }
 
+    function showResult(answerArr){
+        btn1.style.display = "none";
+        btn2.style.display = "none";
+        sharetBtn.style.display = "block";
+
+        const result = parseResult(answerArr);
+
+        body.style.backgroundColor = "#fce56d";
+        bg.src = `./src/image/role-${result}.png`;
+    }
+
     startBtn.addEventListener("click", turnPage);
-    btn1.addEventListener("click", function (){
+    btn1.addEventListener("click", () => {
         turnPage(1)});
-    btn2.addEventListener("click", function (){
+    btn2.addEventListener("click", () => {
         turnPage(2)});
-    sharetBtn.addEventListener("click", () =>{
+    sharetBtn.addEventListener("click", () => {
 
     });
 }
